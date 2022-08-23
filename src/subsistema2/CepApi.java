3b51e47f5ec2cc3f33c58f6ba387e8e6fec421f3
Lucas Natan Camacho <gatlingnoah@gmail.com>
2022-08-23 10:32:29 -0300

    Commit das novas classes

---
+++ src/subsistema2/CepApi.java
@@ -0,0 +1,21 @@
+package subsistema2;
+
+public class CepApi {
+    private static CepApi instancia = new CepApi();
+
+    private CepApi(){
+        super();
+    }
+
+    public static CepApi getInstancia(){
+        return instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Tupã";  
+    }
+
+    public String recuperarEstado(String cep) {
+        return "SP";  
+    }
+}
